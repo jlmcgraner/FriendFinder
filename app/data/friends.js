7d988eed4ee85a91d55050489706d461918b0d78
Jordan McGraner <jlmcgraner@gmail.com>
2020-01-29 06:33:36 -0500

    Initial FriendFinder Project Push

---
+++ app/data/friends.js
@@ -0,0 +1,108 @@
+// ===============================================================================
+// DATA
+// Below data will hold all of the possible friends.
+// ===============================================================================
+
+
+var friends = [
+  {
+    name: "Will Smith",
+    photo: "https://www.tubefilter.com/wp-content/uploads/2019/10/will-smith-tiktok.jpg",
+    scores: [
+      "5",
+      "4",
+      "3",
+      "2",
+      "1",
+      "2",
+      "3",
+      "4",
+      "5",
+      "4"
+    ]
+  },
+  {
+    name: "Dwayne Johnson",
+    photo: "https://i.insider.com/5b462c57f4af9c1a008b45eb?width=1073&format=jpeg",
+    scores: [
+      "5",
+      "3",
+      "5",
+      "3",
+      "5",
+      "3",
+      "5",
+      "3",
+      "5",
+      "3"
+    ]
+  },
+  {
+    name: "Theo Vonn",
+    photo: "https://pbs.twimg.com/profile_images/1106992295780352001/sov3Mgph_400x400.png",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Chris Delia",
+    photo: "https://heavyeditorial.files.wordpress.com/2014/05/cd2.jpg?quality=65&strip=all",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Bryan Callen",
+    photo: "https://m.media-amazon.com/images/M/MV5BMjA4NDU2ODM4MV5BMl5BanBnXkFtZTcwOTc2ODE3Mw@@._V1_UY317_CR1,0,214,317_AL_.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Betty White",
+    photo: "https://ewscripps.brightspotcdn.com/dims4/default/f59954b/2147483647/strip/true/crop/640x360+0+60/resize/1280x720!/quality/90/?url=https%3A%2F%2Fmediaassets.abcactionnews.com%2Fphoto%2F2018%2F01%2F17%2FBetty-white_1516192252889_76094673_ver1.0_640_480.png",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
